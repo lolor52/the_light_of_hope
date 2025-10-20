351a9ca4a41419d0331943528a5be00795530e12
lolor52 <vsekaba@gmail.com>
2025-10-21 01:39:19 +0300

    Rename ticker table and extend model

--- models/ticker.go
+++ models/ticker.go
@@ -2,12 +2,15 @@ package models
 
 import "time"
 
-// TickerVWAPVA описывает данные VWAP/VAL/VAH для торговой сессии конкретного тикера.
-type TickerVWAPVA struct {
+// Ticker описывает агрегированные метрики торговой сессии конкретного тикера.
+type Ticker struct {
 	TradingSessionDate   time.Time `db:"trading_session_date"`
 	TradingSessionActive *bool     `db:"trading_session_active"`
 	Ticker               string    `db:"ticker"`
 	VWAP                 *string   `db:"vwap"`
 	VAL                  *string   `db:"val"`
 	VAH                  *string   `db:"vah"`
+	Liquidity            *string   `db:"liquidity"`
+	Volatility           *string   `db:"volatility"`
+	FlatTrendFilter      *string   `db:"flat_trend_filter"`
 }
